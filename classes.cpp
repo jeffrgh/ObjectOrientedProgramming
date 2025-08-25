112ca74298f2e5ff7ed21794ecf37e1434c16ecc
Jeffrey Gabriel Harris <jeffrey.h-28@scds.saiuniversity.edu.in>
2025-08-25 11:39:25 +0530

    25/08/2025

--- classes.cpp
+++ classes.cpp
@@ -7,19 +7,30 @@ class details
 {
 private:
 	int roll_no;
-	float marks;
+	float marks, m2, m3, m4, m5;
 	string name;
 
 public:
-	void setValue(int rn, float mr, string nm)
+	void setValue(int rn, float mr, float mr2, float mr3, float mr4, float mr5, string nm)
 	{
 		roll_no = rn;
 		marks = mr;
+		m2 = mr2;
+		m3 = mr3;
+		m4 = mr4;
+		m5 = mr5;
 		name = nm;
 	}
 	void getValue()
 	{
-		cout << "Name: " << name << ", Roll NO.: " << roll_no << ", Marks: " << marks;
+		float avg;
+		// cout << marks << endl
+		// 	 << m2 << endl
+		// 	 << m3 << endl
+		// 	 << m4 << endl
+		// 	 << m5 << endl;
+		avg = ((marks + m2 + m3 + m4 + m5) / 5);
+		cout << "Name: " << name << ", Roll NO.: " << roll_no << ", Marks: " << avg;
 	}
 };
 
@@ -29,7 +40,7 @@ int main()
 	// d1.name = "Roxas";
 	// d1.roll_no = 69;
 	// d1.marks = 99.8;
-	d1.setValue(69, 99.8, "Roxas");
+	d1.setValue(69, 99.1, 98.5, 99.9, 97.3, 99.8, "Roxas");
 
 	d1.getValue();
 
