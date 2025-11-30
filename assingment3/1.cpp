47f38b2291d6a4a7879c4ff4d9392df43ed7cb65
Jeffrey GH <hjeff2507@gmail.com>
2025-11-30 09:50:55 +0530

    Fix condition in while loop for right pointer
    I messed up and typed the function wrong.

--- assingment3/1.cpp
+++ assingment3/1.cpp
@@ -63,7 +63,7 @@ vector<numberPair> twoSum(vector<int> nums, int target)
 
 			while (left < right && nums[left] == nums[left + 1])
 				left++;
-			while (right < left && nums[right] == nums[right - 1])
+			while (left < right && nums[right] == nums[right - 1])
 				right--;
 
 			left++;
